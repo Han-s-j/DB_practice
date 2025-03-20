bf979027e946101066fd68f9dd2f48e9d880782a
Han-s-j <78011872+Han-s-j@users.noreply.github.com>
2025-03-20 18:59:16 +0900

    0320

--- members.sql
+++ members.sql
@@ -19,3 +19,70 @@ CREATE TABLE members (
 
 SELECT *
 FROM members;
+
+INSERT INTO members (mem_id, mem_pw, mem_nm)
+VALUES ('nick', '1234', '닉');
+commit;
+
+delete members;
+commit;
+
+SELECT mem_id
+     , mem_pw
+     , mem_nm
+     , mem_addr
+     , profile_img
+FROM members
+WHERE use_yn = 'Y'
+AND mem_id = 'nick'
+AND mem_pw = '1234';
+
+CREATE TABLE boards (
+     board_no NUMBER GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1 NOCACHE) PRIMARY KEY   
+    ,board_title VARCHAR2(1000)             
+    ,mem_id VARCHAR2(100)   NOT NULL
+    ,board_content VARCHAR2(2000)
+    ,creat_dt DATE      DEFAULT SYSDATE
+    ,update_dt DATE     DEFAULT SYSDATE
+    ,use_yn VARCHAR2(1) DEFAULT 'Y'  
+);
+
+INSERT INTO boards (board_title, mem_id, board_content)
+VALUES ('test2','nick','내용입니다.');
+
+delete boards;
+commit;
+
+SELECT a.board_no
+    ,a.board_title
+    ,b.mem_id
+    ,b.mem_nm
+    ,TO_CHAR(a.update_dt,'YYYY/MM/DD HH24:MI:SS')as update_dt
+FROM boards a
+    ,members b
+WHERE a.mem_id = b.mem_id
+AND a.use_yn = 'Y'
+ORDER BY a.update_dt DESC;
+
+-- 상세 조회
+SELECT a.board_no
+    ,a.board_title
+    ,a.board_content
+    ,b.mem_id
+    ,b.mem_nm
+    ,TO_CHAR(a.update_dt,'YYYY/MM/DD HH24:MI:SS')as update_dt
+FROM boards a
+    ,members b
+WHERE a.mem_id = b.mem_id
+AND a.use_yn = 'Y'
+AND a.board_no = '4';
+
+select *
+from boards
+where board_no = '4';
+-- 데이터 체크 후 수정하는 습관!
+update boards
+set board_title = 'bye'
+    , board_content = 'hi hello'
+where board_no = '4';
+
