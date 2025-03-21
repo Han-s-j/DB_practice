198d4f61d6ccd3680f000a532319a4985a10a270
Han-s-j <78011872+Han-s-j@users.noreply.github.com>
2025-03-21 18:00:24 +0900

    0321

--- members.sql
+++ members.sql
@@ -84,5 +84,60 @@ where board_no = '4';
 update boards
 set board_title = 'bye'
     , board_content = 'hi hello'
-where board_no = '4';
+    , update_dt = SYSDATE
+where board_no = '4'
+AND mem_id = 'nick';
+
+-- 댓글 테이블
+CREATE TABLE replys (
+    reply_no NUMBER, -- 자동 증가 없음, 직접 값 입력 필요
+    board_no NUMBER(10),
+    mem_id VARCHAR2(100),
+    reply_content VARCHAR2(1000),
+    reply_date DATE DEFAULT SYSDATE,
+    del_yn VARCHAR2(1) DEFAULT 'N',
+    PRIMARY KEY (board_no, reply_no),
+    CONSTRAINT fk_mem_id FOREIGN KEY (mem_id) REFERENCES members(mem_id) ON DELETE CASCADE, -- 회원 삭제 시 관련 댓글도 삭제
+    CONSTRAINT fk_board_no FOREIGN KEY (board_no) REFERENCES boards(board_no) ON DELETE CASCADE -- 게시글 삭제 시 관련 댓글도 삭제
+);
+
+SELECT *
+FROM replys;
+
+-- 댓글 저장
+INSERT INTO replys (board_no, reply_no, mem_id, reply_content)
+VALUES('4','123456','avo','댓글 테스트');
+INSERT INTO replys (board_no, reply_no, mem_id, reply_content)
+VALUES('4','123457','avo','댓글 테스트2');
+INSERT INTO replys (board_no, reply_no, mem_id, reply_content)
+VALUES('4','123458','nick','댓글 테스트3');
+-- 댓글 1건 조회
+SELECT a.reply_no
+     , a.board_no
+     , b.mem_id
+     , b.mem_nm
+     , a.reply_content
+     ,TO_CHAR(a.reply_date, 'MM/DD HH24:MI') as reply_date
+FROM replys a, members b
+WHERE a.mem_id = b.mem_id
+AND   a.del_yn = 'N'
+AND   a.reply_no = '123457';
+
+-- 댓글 여러건 조회
+SELECT a.reply_no
+     , a.board_no
+     , b.mem_id
+     , b.mem_nm
+     , a.reply_content
+     ,TO_CHAR(a.reply_date, 'MM/DD HH24:MI') as reply_date
+FROM replys a, members b
+WHERE a.mem_id = b.mem_id
+AND   a.del_yn = 'N'
+AND   a.board_no = '4'
+ORDER BY reply_date DESC;
+
+ALTER TABLE boards ADD(del_yn VARCHAR2(2));
+
+UPDATE boards
+SET del_yn = 'N';
 
